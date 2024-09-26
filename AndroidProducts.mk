783ba94c5719849f34db4616baab14e0f1e9cd23
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-26 05:54:32 +0200

    Z60_plus: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: symphony Model: Z60 plus Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Z60_plus.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Z60_plus-user \
+    omni_Z60_plus-userdebug \
+    omni_Z60_plus-eng
